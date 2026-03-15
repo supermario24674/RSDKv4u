cbc123a1dcf8055ff97d4ce672e33d9d23ea1043
supermario24674 <121689771+supermario24674@users.noreply.github.com>
2026-03-14 21:53:06 -0400

    Create Video.hpp

---
+++ RSDKv4/Video.hpp
@@ -0,0 +1,36 @@
+#ifndef VIDEO_H
+#define VIDEO_H
+
+#include "theoraplay.h"
+
+extern int currentVideoFrame;
+extern int videoFrameCount;
+extern int videoWidth;
+extern int videoHeight;
+extern float videoAR;
+
+extern THEORAPLAY_Decoder *videoDecoder;
+extern const THEORAPLAY_VideoFrame *videoVidData;
+extern const THEORAPLAY_AudioPacket *videoAudioData;
+extern THEORAPLAY_Io callbacks;
+
+extern byte videoSurface;
+extern int videoFilePos;
+extern int videoPlaying; // 0 = not playing, 1 = playing OGV, 2 = playing RSV
+extern int vidFrameMS;
+extern int vidBaseticks;
+extern float videoAR;
+
+void PlayVideoFile(char *filepath, int audioTrack);
+void UpdateVideoFrame();
+void PauseVideo();
+void ResumeVideo();
+int ProcessVideo();
+int QuitVideo();
+void StopVideoPlayback();
+
+void InitVideoBuffer(int width, int height);
+void SetupVideoBuffer(int width, int height);
+void CloseVideoBuffer();
+
+#endif
